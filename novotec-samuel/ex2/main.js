2c8173ee4b56b977764dea3e52a3a6f56b208a17
Carolina Reis <crlnrjxxx@gmail.com>
2024-08-07 12:34:08 -0300

    cores

--- novotec-samuel/ex2/main.js
+++ novotec-samuel/ex2/main.js
@@ -1,4 +1,21 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Olá mundo!";
+function atualizar (){
+    let msg = window.document.getElementById('msg')
+    let imagem = window.document.getElementById('img')
+    let data = new Date()
+    let hora = 22//data.getHours()
+    let min = data.getMinutes()
+    msg.innerHTML = `Agora são ${hora} horas e ${min} minutos`
 
-console.log
+    if(hora >= 0 && hora < 12){
+        //bom dia
+        imagem.src ='assets/tarde.png'
+    } else if(hora >= 12 &&  hora <18){
+        //boa tarde
+        imagem.src ='assets/fotor.png'
+
+    } else {
+        imagem.src = 'assets/foto.png'
+        document.body.style.background = '#73626e'
+        document.body.header.style.color = '#fffff2'
+    }
+}
