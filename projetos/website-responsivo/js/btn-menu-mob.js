4d137c2d2dd12dbc97015d72a5fae166072471fe
Carolina Reis <crlnrjxxx@gmail.com>
2024-09-10 12:15:02 -0300

    finalização do website, e coloquei as imagagens do carrosel

--- projetos/website-responsivo/js/btn-menu-mob.js
+++ projetos/website-responsivo/js/btn-menu-mob.js
@@ -1,16 +1,24 @@
 let btnMenuMob = document.querySelector('#btn-menu-mob')
 let line1 = document.querySelector('.line-menumob-1')
 let line2 = document.querySelector('.line-menumob-2')
-let menuMobile = document.querySelector('#menu-mobile')
 
-btnMenuMob.addEventListener('click', ()=>{
+let menuMobile = document.querySelector('#menu-mobile') /*expande o menu mobile*/
+
+let body = document.querySelector('body') /* não vai deixar rolar a pagina  */
+
+btnMenuMob.addEventListener('click', ()=>{ 
     /*cria uma classe dinamica no js*/
-
-    line1.classList.toggle('ativo1')   
-    line2.classList.toggle('ativo2')  
-
     /* o toggle adiciona e remove a classe*/
 
-    menuMobuile.classList.toggle('abrir')
+    /*vai deixar animado fazendo virar um x*/
+    line1.classList.toggle('ativo1')   
+    line2.classList.toggle('ativo2')  
+     
+    /*vai fazer o efeito de expansao do menu mob*/
+    menuMobile.classList.toggle('abrir')
+
+    /* não vai deixar rolar a pagina enquanto o botão estiver aberto*/
+    body.classList.toggle('no-overflow')
+
 })
 
