682406ac28ea43f186dbb41dab7f9f6454c2d26e
Carolina Reis <crlnrjxxx@gmail.com>
2024-10-29 09:08:13 -0300

    não lembro oq é

--- tcc/javascript/text-decoration-como-fazer.js
+++
@@ -1,9 +0,0 @@
-const feito = document.querySelector('.li')
-const feito2 = document.getElementsByName('label')
-
-feito2 = addEventListener('click', ()=>{
-    feito1.classList.toggle('concluido')
-})
-feito.addEventListener('click', ()=>{
-    feito.classList.toggle('concluido')
-})
